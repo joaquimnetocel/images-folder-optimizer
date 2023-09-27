cb958f73e45e53fa0304057a54b14f695ce005bf
joaquimnetocel <joaquim.neto.cel@gmail.com>
2023-09-27 16:27:28 -0300

    npm run lint

--- src/functionExtractNameAndExtension.ts
+++ src/functionExtractNameAndExtension.ts
@@ -1,8 +1,6 @@
 import path from 'path';
 
-export const functionExtractNameAndExtension = function (
-	parFullFileName: string,
-) {
+export const functionExtractNameAndExtension = function (parFullFileName: string) {
 	const arrayReturn = path.basename(parFullFileName || '').split('.');
 	return {
 		stringFileName: arrayReturn[0],
